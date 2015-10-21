d98dbcfef2e6d99e8461980ab2292c9b375d2448
Lindsey <lindseydwhitney@gmail.com>
2015-10-21 11:53:48 -0600

    Used jQuery to obtain values from input fields after pushing button.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+var printWidth300;
+var printHeight300;
+var printWidth240;
+var printHeight240;
+var printWidth150;
+var printHeight150;
+
+var printWidth;
+var printHeight;
+var pixelsAt300;
+var pixelsAt240;
+var pixelsAt150;
+
+
+
+
+function hideCalc () {
+  $(".output").hide();
+}
+
+function revealCalc () {
+  $(".output").show("slow");
+}
+
+$(document).ready(hideCalc);
+
 $(".animsition").animsition();
+
+$( ".button" ).click( function(){
+  var $pixelWidth = $("#widthinpixels").val();
+  var $pixelHeight = $("#heightinpixels").val();
+
+  printWidth300 = $pixelWidth / 300;
+  printHeight300 = $pixelHeight / 300;
+  console.log($pixelWidth);
+  console.log($pixelHeight);
+  console.log(printWidth300);
+  console.log(printHeight300);
+
+  
+
+});
+
+$( "form" ).submit(function( event ) {
+  event.preventDefault();
+});
