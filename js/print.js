569af046f159969d321064256636884286f386de
Lindsey <lindseydwhitney@gmail.com>
2015-11-03 09:55:40 -0700

    Removed orphan from h2 on print.html.

--- js/print.js
+++ js/print.js
@@ -11,6 +11,15 @@ var pixelsAt300;
 var pixelsAt240;
 var pixelsAt150;
 
+$(".add-rules").each(function() {
+  var wordArray = $(this).text().split(" ");
+  if (wordArray.length > 1) {
+    wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
+    wordArray.pop();
+    $(this).html(wordArray.join(" "));
+  }
+});
+
 function hideCalc () {
   $(".output").hide();
 }
